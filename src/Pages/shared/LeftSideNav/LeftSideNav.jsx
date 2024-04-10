539b3b670877ba07316a8bc92a7866065bb59eb3
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-04-10 13:21:01 +0600

    addbthe structcer

--- src/Pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState} from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCateories] = useState([])
+    
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCateories(data))
+    },[])
+    
     return (
-        <div>
-            <h2 className="text-2xl">Left Side</h2>
+        <div className="space-y-6">
+        <h2>All Categories</h2>
+        {
+            categories.map(category => <Link
+            className="block ml-4 text-xl font-semibold "
+                 key={category.id}
+                 to={`/catagory/${category.id}`}
+                 >{category.name}</Link>)
+        }
             
         </div>
     );
--- src/Pages/shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,45 @@
-
-
+import { FaGoogle } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { BsInstagram } from "react-icons/bs";
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right Side </h2>
+         <div className="p-4 space-y-3 flex flex-col"><h2 className="text-3xl w-full">Login With </h2>
+         <button className="btn btn-outline">
+         <FaGoogle />
+  Google
+</button>
+
+         <button className="btn btn-outline">
+         <FaGithub  />
+  Github
+</button>
+</div>
+<div className="p-4 space-y-3 flex flex-col"><h2 className="text-3xl w-full">Find Us On </h2>
+<a className="p-4 flex text-lg items-center border rounded-b-lg " href="">
+<FaFacebook />
+<span>Facebook</span>
+</a>
+<a className="p-4 flex text-lg items-center border ronded " href="">
+<FaXTwitter />
+<span>Twitter</span>
+</a>
+<a className="p-4 flex text-lg items-center border ronded " href="">
+<BsInstagram />
+<span>Instagram</span>
+</a>
+</div>
+<div className="p-4 space-y-3 mb-6">
+  <h2 className="text-3xl">Q Zone</h2>
+  <img src={qzone1} alt="" />
+  <img src={qzone2} alt="" />
+  <img src={qzone3} alt="" />
+</div>
         </div>
     );
 };
